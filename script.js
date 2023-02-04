8b5c794e2c2563296d31f4c5d664bdc39d56879f
Ronaldo Noberto <103396752+Ronaldo913@users.noreply.github.com>
2023-02-04 02:41:50 +0000

    Add loader

--- script.js
+++ script.js
@@ -53,3 +53,11 @@ $(document).ready(function() {
     }
   });
 });
+
+var i = setInterval(function () {  
+  clearInterval(i);
+  
+  // O código desejado é apenas isto:
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("container").style.display = "inline";
+}, 3100);
